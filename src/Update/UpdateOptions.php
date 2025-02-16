fe3505020c149aacff928324e5a83fb24b98e042
david_smith <david_smith@sweetwater.com>
2025-02-16 14:01:22 -0500

    feat/annotator Updated `src/Update/UpdateOptions.php`.

--- src/Update/UpdateOptions.php
+++ src/Update/UpdateOptions.php
@@ -9,12 +9,30 @@ class UpdateOptions
 {
     use DataModel;
 
+    /**
+     * @link https://github.com/zero-to-prod/docblock-annotator-cli
+     */
     public const visibility = 'visibility';
+    /**
+     * @link https://github.com/zero-to-prod/docblock-annotator-cli
+     */
     public array $visibility = [Annotator::public];
 
+    /**
+     * @link https://github.com/zero-to-prod/docblock-annotator-cli
+     */
     public const members = 'members';
+    /**
+     * @link https://github.com/zero-to-prod/docblock-annotator-cli
+     */
     public array $members = [Annotator::method, Annotator::property, Annotator::constant];
 
+    /**
+     * @link https://github.com/zero-to-prod/docblock-annotator-cli
+     */
     public const recursive = 'recursive';
+    /**
+     * @link https://github.com/zero-to-prod/docblock-annotator-cli
+     */
     public bool $recursive = true;
 }
