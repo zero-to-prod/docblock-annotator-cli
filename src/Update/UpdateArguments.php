d146b646b7736475a431b4359c37d9fa3a024b24
david_smith <david_smith@sweetwater.com>
2025-02-16 14:01:22 -0500

    feat/annotator Updated `src/Update/UpdateArguments.php`.

--- src/Update/UpdateArguments.php
+++ src/Update/UpdateArguments.php
@@ -8,9 +8,21 @@ class UpdateArguments
 {
     use DataModel;
 
+    /**
+     * @link https://github.com/zero-to-prod/docblock-annotator-cli
+     */
     public const directory = 'directory';
+    /**
+     * @link https://github.com/zero-to-prod/docblock-annotator-cli
+     */
     public string $directory;
 
+    /**
+     * @link https://github.com/zero-to-prod/docblock-annotator-cli
+     */
     public const comments = 'comments';
+    /**
+     * @link https://github.com/zero-to-prod/docblock-annotator-cli
+     */
     public array $comments;
 }
