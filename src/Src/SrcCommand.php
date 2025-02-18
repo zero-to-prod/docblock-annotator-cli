d978c59b006a04a2ff04950999b4ae9109880016
david_smith <david_smith@sweetwater.com>
2025-02-17 20:02:20 -0500

    doc/update Updated `src/Src/SrcCommand.php`.

--- src/Src/SrcCommand.php
+++ src/Src/SrcCommand.php
@@ -4,11 +4,12 @@ namespace Zerotoprod\DocblockAnnotatorCli\Src;
 
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
+/**
+ * @link https://github.com/zero-to-prod/docblock-annotator-cli
+ */
 #[AsCommand(
     name: SrcCommand::signature,
     description: 'Project source link'
