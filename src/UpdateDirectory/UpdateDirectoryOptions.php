309f379e47417cc6715f8643d368610d2c09430d
david_smith <david_smith@sweetwater.com>
2025-02-20 15:49:26 -0500

    v4 Updated `src/UpdateDirectory/UpdateDirectoryOptions.php`.

--- src/UpdateDirectory/UpdateDirectoryOptions.php
+++ src/UpdateDirectory/UpdateDirectoryOptions.php
@@ -7,6 +7,7 @@ use Zerotoprod\DocblockAnnotator\Modifier;
 use Zerotoprod\DocblockAnnotator\Statement;
 
 /**
+ * @internal
  * @link https://github.com/zero-to-prod/docblock-annotator-cli
  */
 class UpdateDirectoryOptions
@@ -14,30 +15,36 @@ class UpdateDirectoryOptions
     use DataModel;
 
     /**
+     * @internal
      * @link https://github.com/zero-to-prod/docblock-annotator-cli
      */
     public const modifiers = 'modifiers';
     /**
      * @var Modifier[]
+     * @internal
      * @link https://github.com/zero-to-prod/docblock-annotator-cli
      */
     public array $modifiers = [];
 
     /**
+     * @internal
      * @link https://github.com/zero-to-prod/docblock-annotator-cli
      */
     public const statements = 'statements';
     /**
      * @var Statement[]
+     * @internal
      * @link https://github.com/zero-to-prod/docblock-annotator-cli
      */
     public array $statements = [];
 
     /**
+     * @internal
      * @link https://github.com/zero-to-prod/docblock-annotator-cli
      */
     public const recursive = 'recursive';
     /**
+     * @internal
      * @link https://github.com/zero-to-prod/docblock-annotator-cli
      */
     public bool $recursive = true;
