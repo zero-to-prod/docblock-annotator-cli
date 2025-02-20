9f366e6b3f2f5d97484128ba29fc77aeb95c661f
david_smith <david_smith@sweetwater.com>
2025-02-20 15:40:33 -0500

    v4 Updated `src/UpdateDirectory/UpdateDirectoryCommand.php`.

--- src/UpdateDirectory/UpdateDirectoryCommand.php
+++ src/UpdateDirectory/UpdateDirectoryCommand.php
@@ -9,8 +9,9 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Zerotoprod\DocblockAnnotator\Annotator;
 use Zerotoprod\DocblockAnnotator\DocblockAnnotator;
+use Zerotoprod\DocblockAnnotator\Modifier;
+use Zerotoprod\DocblockAnnotator\Statement;
 
 /**
  * @link https://github.com/zero-to-prod/docblock-annotator-cli
@@ -32,21 +33,40 @@ class UpdateDirectoryCommand extends Command
         $Args = UpdateDirectoryArguments::from($input->getArguments());
         $Options = UpdateDirectoryOptions::from($input->getOptions());
 
-        DocblockAnnotator::updateDirectory(
-            $Args->directory,
-            $Args->comments,
-            $Options->visibility,
-            $Options->members,
-            static function (string $file) {
-                echo $file.PHP_EOL;
+        /** @var array{success: string[], failure: string[]} $messages */
+        $messages = ['success' => [], 'failure' => []];
+        $DocblockAnnotator = new DocblockAnnotator(
+            $Options->modifiers,
+            $Options->statements,
+            function (string $file) use (&$messages) {
+                return $messages['success'][] = $file;
             },
-            static function (Throwable $Throwable) {
-                echo $Throwable->message().PHP_EOL;
-            },
-            $Options->recursive
+            function (Throwable $e) use (&$messages) {
+                return $messages['failure'][] = $e->message();
+            }
         );
 
-        return Command::SUCCESS;
+        try {
+            $DocblockAnnotator->updateDirectory(
+                $Args->comments,
+                $Args->directory,
+                $Options->recursive
+            );
+        } catch (\Throwable $e) {
+            $output->writeln($e->getMessage());
+
+            return Command::FAILURE;
+        }
+
+        foreach ($messages as $list) {
+            foreach ($list as $message) {
+                $output->writeln($message);
+            }
+        }
+
+        return empty($messages['failure'])
+            ? Command::SUCCESS
+            : Command::FAILURE;
     }
 
     /**
@@ -56,8 +76,8 @@ class UpdateDirectoryCommand extends Command
     {
         $this->addArgument(UpdateDirectoryArguments::directory, InputArgument::REQUIRED, 'The directory to update php files.');
         $this->addArgument(UpdateDirectoryArguments::comments, InputArgument::IS_ARRAY, "The comments to add to the docblock. (e.g. 'app:foo bar baz' = ['bar', 'baz'])");
-        $this->addOption(UpdateDirectoryOptions::visibility, null, InputOption::VALUE_IS_ARRAY | InputOption::VALUE_OPTIONAL, 'The visibility of the member: public, private, protected. (e.g. --visibility=public --visibility=private --visibility=protected)', [Annotator::public]);
-        $this->addOption(UpdateDirectoryOptions::members, null, InputOption::VALUE_IS_ARRAY | InputOption::VALUE_OPTIONAL, 'The kinds of elements to annotate: method, property, constant, class. (e.g. --members=method --members=property --members=constant --members=class --members=enum --members=enum_case --members=interface --members=trait)', [Annotator::method, Annotator::property, Annotator::constant, Annotator::class_, Annotator::enum, Annotator::enum_case, Annotator::interface_, Annotator::trait_]);
+        $this->addOption(UpdateDirectoryOptions::modifiers, null, InputOption::VALUE_IS_ARRAY | InputOption::VALUE_OPTIONAL, 'The modifier of the member: public, private, protected. (e.g. --modifier=public --modifier=private --modifier=protected)', [Modifier::public]);
+        $this->addOption(UpdateDirectoryOptions::statements, null, InputOption::VALUE_IS_ARRAY | InputOption::VALUE_OPTIONAL, 'The statements to annotate: class_method, const, class, class_const, enum_case, enum, function, trait, property, interface. (e.g. --statements=class_method --statements=const --statements=class --statements=class_const --statements=enum_case --statements=enum --statements=function --statements=trait --statements=property --statements=interface)', [Statement::ClassMethod, Statement::Const_, Statement::Class_, Statement::ClassConst, Statement::EnumCase, Statement::Enum_, Statement::Function_, Statement::Trait_, Statement::Property, Statement::Interface_,]);
         $this->addOption(UpdateDirectoryOptions::recursive, null, InputOption::VALUE_NEGATABLE, 'Recursively searches for files. (e.g. --recursive --no-recursive) ');
     }
 }
