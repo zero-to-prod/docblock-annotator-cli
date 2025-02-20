c8701d17426ad259a538a4047c960bdc34f2b66c
david_smith <david_smith@sweetwater.com>
2025-02-20 15:49:26 -0500

    v4 Updated `src/UpdateDirectory/UpdateDirectoryArguments.php`.

--- src/UpdateDirectory/UpdateDirectoryArguments.php
+++ src/UpdateDirectory/UpdateDirectoryArguments.php
@@ -5,6 +5,7 @@ namespace Zerotoprod\DocblockAnnotatorCli\UpdateDirectory;
 use Zerotoprod\DataModel\DataModel;
 
 /**
+ * @internal
  * @link https://github.com/zero-to-prod/docblock-annotator-cli
  */
 class UpdateDirectoryArguments
@@ -12,19 +13,23 @@ class UpdateDirectoryArguments
     use DataModel;
 
     /**
+     * @internal
      * @link https://github.com/zero-to-prod/docblock-annotator-cli
      */
     public const directory = 'directory';
     /**
+     * @internal
      * @link https://github.com/zero-to-prod/docblock-annotator-cli
      */
     public string $directory;
 
     /**
+     * @internal
      * @link https://github.com/zero-to-prod/docblock-annotator-cli
      */
     public const comments = 'comments';
     /**
+     * @internal
      * @link https://github.com/zero-to-prod/docblock-annotator-cli
      */
     public array $comments;
